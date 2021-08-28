16d90775f9bd42b8f32a4ec01c2f500b92ce1251
Yovana <dorabarriosleal@alumni.usp.br>
2021-08-28 08:56:25 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,181 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import { Title, Title2, FormInput, FormInput2, FormInput3, FormInput4, Body } from './styles';
+import axios from 'axios';
 
-function App() {
+
+
+const App = () =>{
+
+  const fetchAddress = async() => {
+    const address = await axios.get(`https://viacep.com.br/ws/${form.cep}/json/`);
+    setform({ ...form, endereco: address.data.logradouro });
+  };
+
+  const createCandidato = async (candidato) => {
+  // quando subir o backend vai ter uma url que tem que colocar do localhost
+    const user = await axios.post('http://localhost:5000/registro', form);
+    if(user.status === 200){
+      alert('Seu curriculo foi registrado com sucesso');
+    } else{
+      alert('Deu errado');
+    }    
+  };
+
+  const [form, setform] = useState({
+    //igual a back end
+    nome: '',
+    cargoPretendido: '',
+    mes: '',
+    dia: '',
+    ano: '',
+    estadoCivil: '',
+    sexo: '',
+    email: '',
+    cep: '',
+    endereco: '',
+    bairro: '',
+    cidade: '',
+    celular: '',
+    telefoneFixo1: '',
+    telefoneFixo2: '',
+    contato: '',
+    identidade: '',
+    cpf: '',
+    veiculo: '',
+    habilitacao: ''   
+
+  });
+  
+  useEffect(()=>{
+    console.log(form);
+  }, [form])
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <Body>    
+      <div>      
+        <Title> FORMULARIO </Title>     
+      </div>     
+         
+      <container>
+      <div>      
+       <Title2> DADOS PESSOAIS </Title2>   
+      </div>
+        <label> Nome Completo * </label>
+        <FormInput onChange={(e) => {
+          setform({ ...form, nome: e.target.value});
+        }}value= {form.nome}></FormInput>
+        
+        <label> Cargo Pretendido </label>
+        <FormInput onChange={(e) => {
+          setform({ ...form, cargoPretendido: e.target.value});
+        }}value= {form.cargoPretendido}></FormInput>      
+      </container>
+    
+      <div>        
+        <label> Mes </label>
+        <FormInput2 onChange={(e) => {
+          setform({ ...form, mes: e.target.value});
+        }}value= {form.mes}></FormInput2>
+        <label> Dia </label>
+        <FormInput2 onChange={(e) => {
+          setform({ ...form, dia: e.target.value});
+        }}value= {form.dia}></FormInput2>
+        <label> Ano </label>
+        <FormInput2 onChange={(e) => {
+          setform({ ...form, ano: e.target.value});
+        }}value= {form.ano}></FormInput2>
+
+        <label> Estado Civil </label>
+        <FormInput3 onChange={(e) => {
+          setform({ ...form, estadoCivil: e.target.value});
+        }}value= {form.estadoCivil}></FormInput3>
+
+        <label> Sexo </label>
+        <FormInput3 onChange={(e) => {
+          setform({ ...form, sexo: e.target.value});
+        }}value= {form.sexo}></FormInput3>
+      </div>
+    
+      <div>
+      <label> Endereço * </label>
+        <FormInput4 onChange={(e) => {
+          setform({ ...form, endereco: e.target.value});
+        }}value= {form.endereco}></FormInput4>
+      </div>
+
+      <div>
+      <label> Bairro </label>
+        <FormInput3 onChange={(e) => {
+          setform({ ...form, bairro: e.target.value});
+        }}value= {form.bairro}></FormInput3>
+      <label> Cidade </label>
+        <FormInput3 onChange={(e) => {
+          setform({ ...form, cidade: e.target.value});
+        }}value= {form.cidade}></FormInput3>
+      <label> CEP </label>
+        <FormInput3 onChange={(e) => {
+          setform({ ...form, cep: e.target.value});
+        }}value= {form.cep}></FormInput3>
+      </div>
+
+      <div>
+      <label> Celular </label>
+        <FormInput3 onChange={(e) => {
+          setform({ ...form, celular: e.target.value});
+        }}value= {form.celular}></FormInput3>
+      <label> Telefone Fixo 1 </label>
+        <FormInput3 onChange={(e) => {
+          setform({ ...form, telefoneFixo1: e.target.value});
+        }}value= {form.telefoneFixo1}></FormInput3>
+      <label> Telefone Fixo 2 </label>
+        <FormInput3 onChange={(e) => {
+          setform({ ...form, telefoneFixo2: e.target.value});
+        }}value= {form.telefoneFixo2}></FormInput3>
+      </div>
+
+      <div>
+      <label> Contato </label>
+        <FormInput onChange={(e) => {
+          setform({ ...form, contato: e.target.value});
+        }}value= {form.contato}></FormInput>
+      <label> E-mail </label>
+        <FormInput onChange={(e) => {
+          setform({ ...form, email: e.target.value});
+        }}value= {form.email}></FormInput>
+      </div>
+
+      <label> 
+        <Title2> DOCUMENTOS </Title2>         
+      </label>
+      <div>
+      <label> Identidade </label>
+        <FormInput3 onChange={(e) => {
+          setform({ ...form, identidade: e.target.value});
+        }}value= {form.identidade}></FormInput3>
+      <label> CPF </label>
+        <FormInput3 onChange={(e) => {
+          setform({ ...form, cpf: e.target.value});
+        }}value= {form.cpf}></FormInput3>
+      </div>
+      <div>
+      <label> Possui Veículo </label>
+        <FormInput3 onChange={(e) => {
+          setform({ ...form, veiculo: e.target.value});
+        }}value= {form.veiculo}></FormInput3>
+      <label> Habilitação </label>
+        <FormInput3 onChange={(e) => {
+          setform({ ...form, habilitacao: e.target.value});
+        }}value= {form.habilitacao}></FormInput3>
+      </div>
+
+      <div>
+        <button onClick={() => createCandidato()}> ENVIAR </button>
+        <button onClick={() => fetchAddress()}> Procurar CEP </button>
+      </div>  
+      
+    </Body>
   );
 }
 
---
+++ src/styles.js
@@ -0,0 +1,87 @@
+import styled from 'styled-components';
+
+export const Body = styled.body`
+    background-color: #DCDCDC;
+    padding: 5px;
+    border-radius: 5px 5px 5px 5px;
+    margin-top: 1px;
+    margin-right: 1x;
+    margin-bottom: 1px;
+    margin-left: 1px;
+    display: flow-root;
+
+`
+
+//formato da pagina
+export const Title = styled.h1`
+    font-size: 20px;
+    text-align: center;
+    color: black;
+`;
+
+
+export const Title2 = styled.h2`
+    font-size: 16px;
+    text-align: left;
+    color: black;
+    text-decoration: underline;
+    
+`;
+
+export const FormInput = styled.input`
+    border: 1px solid #696969;
+    padding: 5px;
+    border-radius: 5px 5px 5px 5px;
+    margin-top: 5px;
+    margin-right: 10px;
+    margin-bottom: 5px;
+    font-size: 15px;
+    border-color: #000;
+    width: 200px;
+    `;
+
+export const FormInput2 = styled.input`
+    border: 1px solid #696969;
+    padding: 5px;
+    border-radius: 5px 5px 5px 5px;
+    margin-top: 5px;
+    margin-right: 5px;
+    margin-bottom: 5px;
+    font-size: 15px;
+    border-color: #000;
+    width: 25px;
+    `;
+
+export const FormInput3 = styled.input`
+    border: 1px solid #696969;
+    padding: 5px;
+    border-radius: 5px 5px 5px 5px;
+    margin-top: 5px;
+    margin-right: 10px;
+    margin-left: 5px;
+    margin-bottom: 5px;
+    font-size: 15px;
+    border-color: #000;
+    width: 120px;
+    `;
+
+export const FormInput4 = styled.input`
+    border: 1px solid #696969;
+    padding: 5px;
+    border-radius: 5px 5px 5px 5px;
+    margin-top: 5px;
+    margin-right: 5px;
+    margin-left: 5px;
+    margin-bottom: 5px;
+    font-size: 15px;
+    border-color: #000;
+    width: 300px;
+    `;
+
+
+/*const FormButton = styled.button`
+   font-size: 12px;
+   background: transparent;
+   border: 1px solid #CCC;
+   color: #ccc;
+`;*/
